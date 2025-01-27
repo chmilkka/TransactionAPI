e4287b0eccc5270a6804897b312aaf850f2931c4
chmilkka <mishachmil228335@gmail.com>
2025-01-28 01:10:54 +0200

    added application db context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransactionAPI.Models;
+
+namespace TransactionAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Transaction>()
+                .Property(t => t.Id)
+                .HasColumnName("transaction_id");
+
+            modelBuilder.Entity<Transaction>()
+                .Property(t => t.Name)
+                .HasColumnName("name");
+
+            modelBuilder.Entity<Transaction>()
+                .Property(t => t.Email)
+                .HasColumnName("email");
+
+            modelBuilder.Entity<Transaction>()
+                .Property(t => t.Amount)
+                .HasColumnName("amount")
+                .HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Transaction>()
+                .Property(t => t.TransactionDate)
+                .HasColumnName("transaction_date");
+
+            modelBuilder.Entity<Transaction>()
+                .Property(t => t.ClientLocation)
+                .HasColumnName("client_location");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using TransactionAPI.Data;
+
 namespace TransactionAPI
 {
     public class Program
@@ -8,6 +11,8 @@ namespace TransactionAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
