ba6831463075d8643d775f3a44fb59f65f5f45cf
chmilkka <mishachmil228335@gmail.com>
2025-01-28 00:04:54 +0200

    created transaction model

---
+++ Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace TransactionAPI.Models
+{
+    public class Transaction
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string ClientLocation { get; set; }
+    }
+}
