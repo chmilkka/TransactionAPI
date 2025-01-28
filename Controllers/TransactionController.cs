3ad6471e4398a8e5eaf4807afe8c42cdceb75e53
chmilkka <mishachmil228335@gmail.com>
2025-01-28 14:15:22 +0200

    created transaction controller

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,33 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Formats.Asn1;
+using System.Globalization;
+using TransactionAPI.Interfaces;
+using TransactionAPI.Models;
+
+namespace TransactionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController(ITransactionService transactionService) : ControllerBase
+    {       
+        [HttpPost("import")]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> ImportTransactionsFromCsv(IFormFile file)
+        {
+            try
+            {
+                await transactionService.ImportTransactionsAsync(file);
+
+                return Ok("Transactions imported successfully.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error processing file: {ex.Message}");
+            }
+        }
+    }
+}
