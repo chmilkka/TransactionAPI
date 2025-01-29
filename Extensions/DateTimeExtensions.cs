9afa184892529fbe968252fc40b2b8c7ed9b28bb
chmilkka <mishachmil228335@gmail.com>
2025-01-29 17:33:29 +0200

    created an extension method to format the datetime

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TransactionAPI.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToDatabaseDateTimeFormat(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
