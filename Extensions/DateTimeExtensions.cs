1b7f4b5910ee5633ce11e37a3caad473128f7b31
chmilkka <mishachmil228335@gmail.com>
2025-01-31 12:11:35 +0200

    added exception handling

--- Extensions/DateTimeExtensions.cs
+++ Extensions/DateTimeExtensions.cs
@@ -4,6 +4,11 @@
     {
         public static string ToDatabaseDateTimeFormat(this DateTime dateTime)
         {
+            if (dateTime == DateTime.MinValue || dateTime == DateTime.MaxValue)
+            {
+                throw new ArgumentException("Invalid DateTime value.");
+            }
+
             return dateTime.ToString("yyyy-MM-dd HH:mm:ss");
         }
     }
