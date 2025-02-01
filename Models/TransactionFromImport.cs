de044ae8b588d051e65e5e6cffaeab94373a17da
chmilkka <mishachmil228335@gmail.com>
2025-02-02 00:13:41 +0200

    added transaction model from import

---
+++ Models/TransactionFromImport.cs
@@ -0,0 +1,12 @@
+﻿namespace TransactionAPI.Models
+{
+    public class TransactionFromImport
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string ClientLocation { get; set; }
+    }
+}
